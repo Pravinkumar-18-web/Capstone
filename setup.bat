b499fbc614e7ddc6589db1f5c0fee3e74c211391
Pravinkumar-18-web <pravinpk181998@gmai.com>
2025-01-05 01:43:26 +0530

    add requirement txt

--- setup.bat
+++ setup.bat
@@ -3,7 +3,7 @@ set AUTH0_DOMAIN=dev-pkprojects.us.auth0.com
 set ALGORITHMS=RS256
 set API_AUDIENCE=capstone
 
-set DATABASE_URL=postgresql://postgres:1898@localhost:5432/capstone
+set DATABASE_URL=postgres://ucd915lid4ku9f:pc9cc106b204f19cb790a3bc7b7626017cb5610ac652fb095efe5d72240e8fea0@ceqbglof0h8enj.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dfjn0ap7i5suu4
 set DATABASE_URL_Test=postgresql://postgres:1898@localhost:5432/capstone_test
 set FLASK_APP=flaskr
 set FLASK_DEBUG=True
