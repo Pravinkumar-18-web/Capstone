368a635402eb0300315152cbb752351ae4d72792
Pravinkumar-18-web <pravinpk181998@gmai.com>
2025-01-04 23:28:59 +0530

    final commits

--- manage.py
+++ manage.py
@@ -1,15 +1,10 @@
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
-
-from flaskr import create_app
+from flask import Flask
+from flask_migrate import Migrate
 from models import db
+from flaskr import create_app
 
 app = create_app()
 migrate = Migrate(app, db)
-manager = Manager(app)
 
-manager.add_command('db', MigrateCommand)
-
-
-if __name__ == '__main__':
-    manager.run()
+if __name__ == "__main__":
+    app.run()
