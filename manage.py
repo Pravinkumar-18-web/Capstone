4f7824db16d66ba10bf07757757234598ad3a70a
Pravinkumar-18-web <pravinpk181998@gmai.com>
2025-01-04 23:49:43 +0530

    add gunicorn

--- manage.py
+++ manage.py
@@ -1,10 +1,20 @@
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from flask import Flask
-from flask_migrate import Migrate
-from models import db
 from flaskr import create_app
+from models import db
 
+# Create the app instance
 app = create_app()
+
+# Set up the migration environment
 migrate = Migrate(app, db)
 
-if __name__ == "__main__":
-    app.run()
+# Set up the manager for running the commands
+manager = Manager(app)
+
+# Add the database migration commands to the manager
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
