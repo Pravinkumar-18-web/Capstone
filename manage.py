7204c0c1eb2f21d451e646c3b7d217d88945fb49
Pravinkumar-18-web <pravinpk181998@gmai.com>
2025-01-05 01:13:27 +0530

    Fix compatibility

--- manage.py
+++ manage.py
@@ -1,5 +1,4 @@
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate
 from flask import Flask
 from flaskr import create_app
 from models import db
@@ -10,11 +9,5 @@ app = create_app()
 # Set up the migration environment
 migrate = Migrate(app, db)
 
-# Set up the manager for running the commands
-manager = Manager(app)
-
-# Add the database migration commands to the manager
-manager.add_command('db', MigrateCommand)
-
 if __name__ == '__main__':
-    manager.run()
+    app.run()
